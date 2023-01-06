bee35ee76130e774d37fd50cd6fdc007f0a33143
davidPRIGODIN <79266421+davidPRIGODIN@users.noreply.github.com>
2023-01-06 14:42:38 +0100

    Create launch
    Contains all scripts to launch TsunamiLab

---
+++ launch/microsoft_edge_process.ps1
@@ -0,0 +1,13 @@
+﻿start microsoft-edge:http://localhost:8000/
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('Google - Microsoft Edge')
+sleep 2
+$wshell.SendKeys('{F11}')
+
+
+
+
+
+
+
+
